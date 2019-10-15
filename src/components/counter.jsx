25e4c3fc16031454f507775ecddad8aa5eacff6a
Merle Jansen <merlevanjansen@gmail.com>
2019-10-15 12:41:09 +0200

    delete button added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3'],
+    value: this.props.counter.value,
+    // tags: ['tag1', 'tag2', 'tag3'],
   };
 
   handleIncrement = () => {
-    this.setState ({count: this.state.count + 1});
-    console.log ('Increment clicked', this.state.count);
+    this.setState ({value: this.state.value + 1});
+    console.log ('Increment clicked', this.state.value);
   };
 
   render () {
     return (
       <React.Fragment>
+        {/* {this.props.children} */}
         <span className={this.getBadgeClasses ()}>
           {this.formatCount ()}
         </span>
@@ -23,14 +24,20 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.renderTags ()}
+        {/* {this.renderTags ()} */}
+        <button
+          onClick={() => this.props.onDelete (this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
 
       </React.Fragment>
     );
   }
   formatCount = () => {
-    const {count} = this.state;
-    return count === 0 ? 'Zero' : count;
+    const {value} = this.state;
+    return value === 0 ? 'Zero' : value;
   };
 
   renderTags () {
@@ -50,7 +57,7 @@ class Counter extends Component {
 
   getBadgeClasses () {
     let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 }
